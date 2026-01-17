4fb26ff455c2ae570d66dadb73627f5ac0125fc6
Krutarth-Aperions <krutarth.aperions@gmail.com>
2026-01-17 11:14:17 +0530

    Add .env file

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^!^7-^ajb9njoctb6$oyhczoanmyj-k05rr1$krju*&uz=^i_b'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
